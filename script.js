e6b06f25bd8ef3bffb3b9928f65b34852b8a57b6
KAIAN FERREIRA DOS SANTOS <1911511535@acad.dvuadmin.net>
2019-04-04 21:58:35 -0300

    update 04/04

---
+++ script.js
@@ -0,0 +1,17 @@
+function abreModalPersoanagem(personagem){
+    $(".ui.modal."+personagem).modal({
+        inverted:true
+    }).modal("show");
+}
+window.onload = setInterval("console.log('teste')",10000)
+
+window.onload = setInterval(() => {
+    hora = new Date
+    console.log(hora.getHours());
+},5000)
+
+
+window.onload = setInterval(function(){
+
+})
+
